292c13a7b36922e11b098654e14321ef22a993df
Veron <rohit.raja56@gmail.com>
2014-11-24 22:13:51 +0530

    hi

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -11,25 +11,40 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/demo")
-@Produces(MediaType.APPLICATION_JSON)
 public class TimeService {
 
     @GET
     @Path("/time")
+    @Produces(MediaType.APPLICATION_JSON)
     public Time get() {
         return new Time();
     }
     
     @GET
     @Path("/test")
+    @Produces(MediaType.APPLICATION_JSON)
     public String Test()
     {
-    	return "Hi This is Rohit ";
+    	return "Hi This is Mohit ";
     	
     }
     
+    
+    
+    @GET
+    @Path("/test")
+    @Produces(MediaType.TEXT_HTML)
+
+    public String Test2()
+    {
+    	return "Hi This is Amresh";
+    	
+    }
+    
+    
     @PUT
     @Path("/HiThere/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
     public String HiThere(@PathParam ("name") String name)
     {
     	return "Hi " + name + "This is Rohit ";
